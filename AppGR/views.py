cb421502dce7d14c1b799462bf8f28b9bb6c1a02
LizethGarciaRavazze <lgarciaravazze@gmail.com>
2022-09-14 16:19:01 -0500

    Version corregida final

--- AppGR/views.py
+++ AppGR/views.py
@@ -7,22 +7,22 @@ from AppGR.forms import SombrasForm, BaseForm, BrochasForm
 
 def inicio(request):
 
-      return render(request, "AppGR/inicio.html")
+      return render(request, "inicio.html")
 
 
 def sombras(request):
       sombras = Sombras.objects.all()
-      return render(request, "AppGR/sombras.html", {'sombras': sombras})
+      return render(request, "sombras.html", {'sombras': sombras})
 
 
 def base(request):
-      base = base.objects.all()
-      return render(request, "AppGR/base.html", {'base': base})
+      base = Base.objects.all()
+      return render(request, "base.html", {'base': base})
 
 
 def brochas(request):
-      brochas = brochas.objects.all()
-      return render(request, "AppGR/brochas.html", {'brochas': brochas})
+      brochas = Brochas.objects.all()
+      return render(request, "brochas.html", {'brochas': brochas})
 
 
 def sombras_formulario(request):
@@ -33,15 +33,15 @@ def sombras_formulario(request):
                   data = formulario.cleaned_data
                   sombras = Sombras(codigo=data['codigo'], color=data['color'], tipo=data['tipo']  )
                   sombras.save()
-                  return render(request, "AppGR/inicio.html", {"exitoso": True})
+                  return render(request, "inicio.html", {"exitoso": True})
       else:  
             formulario= SombrasForm()  
-      return render(request, "AppGR/form_sombras.html", {"formulario": formulario})
+      return render(request, "form_sombras.html", {"formulario": formulario})
 
 
 
 def busqueda_sombras(request):
-      return render(request, "AppGR/form_busqueda_sombras.html")
+      return render(request, "form_busqueda_sombras.html")
 
 
 
@@ -49,9 +49,9 @@ def buscar_sombras(request):
       if request.GET["codigo"]:
             codigo = request.GET["codigo"]
             sombras = Sombras.objects.filter(codigo__icontains=codigo)
-            return render(request, "AppGR/sombras.html", {'sombras': sombras})
+            return render(request, "sombras.html", {'sombras': sombras})
       else:
-            return render(request, "AppGR/sombras.html", {'sombras': []})
+            return render(request, "sombras.html", {'sombras': []})
 
 def base_formulario(request):
       if request.method == 'POST':
@@ -61,21 +61,21 @@ def base_formulario(request):
                   data = formulario1.cleaned_data
                   base = Base(codigo=data['codigo'], tono=data['tono'], cobertura=data['cobertura']  )
                   base.save()
-                  return render(request, "AppGR/inicio.html", {"exitoso": True})
+                  return render(request, "inicio.html", {"exitoso": True})
       else:  
             formulario1= BaseForm() 
-      return render(request, "AppGR/form_base.html", {"formulario": formulario1})
+      return render(request, "form_base.html", {"formulario": formulario1})
 
 def busqueda_base(request):
-            return render(request, "AppGR/form_busqueda_base.html")
+            return render(request, "form_busqueda_base.html")
 
 def buscar_base(request):
       if request.GET["codigo"]:
             codigo = request.GET["codigo"]
             base = Base.objects.filter(codigo__icontains=codigo)
-            return render(request, "AppGR/base.html", {'base': base})
+            return render(request, "base.html", {'base': base})
       else:
-            return render(request, "AppGR/base.html", {'base': []})
+            return render(request, "base.html", {'base': []})
 
 def brochas_formulario(request):
       if request.method == 'POST':
@@ -85,18 +85,18 @@ def brochas_formulario(request):
                   data = formulario2.cleaned_data
                   brochas = Brochas(numero=data['numero'], clase=data['clase'] )
                   brochas.save()
-                  return render(request, "AppGR/inicio.html", {"exitoso": True})
+                  return render(request, "inicio.html", {"exitoso": True})
       else:  
             formulario2= BrochasForm()  
-      return render(request, "AppGR/form_brochas.html", {"formulario": formulario2})
+      return render(request, "form_brochas.html", {"formulario": formulario2})
 
 def busqueda_brochas(request):
-            return render(request, "AppGR/form_busqueda_brochas.html")
+            return render(request, "form_busqueda_brochas.html")
 
 def buscar_brochas(request):
       if request.GET["numero"]:
             numero = request.GET["numero"]
             brochas = Brochas.objects.filter(numero__icontains=numero)
-            return render(request, "AppGR/brochas.html", {'brochas': brochas})
+            return render(request, "brochas.html", {'brochas': brochas})
       else:
-            return render(request, "AppGR/brochas.html", {'brochas': []})
+            return render(request, "brochas.html", {'brochas': []})
